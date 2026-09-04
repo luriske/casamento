7f1ef909c795b9ceaa99541e231464fee7049872
luriske <78824557+luriske@users.noreply.github.com>
2026-09-04 19:34:17 -0300

    Consolida imagens dos presentes em uma unica API

---
+++ api/gift.js
@@ -0,0 +1,13 @@
+const handlers={
+  '1':require('../lib/gifts-01'),'2':require('../lib/gifts-01'),
+  '3':require('../lib/coffee-local'),'4':require('../lib/robot-local'),
+  '5':require('../lib/gifts-03'),'6':require('../lib/gifts-03'),
+  '7':require('../lib/gifts-04'),'8':require('../lib/gifts-04'),
+  '9':require('../lib/gifts-05'),'10':require('../lib/gifts-05'),
+  '11':require('../lib/gifts-06'),'12':require('../lib/gifts-06'),
+  '13':require('../lib/gifts-07'),'14':require('../lib/gifts-07'),
+  '15':require('../lib/gifts-08'),'16':require('../lib/gifts-08'),
+  '17':require('../lib/gift-17'),'18':require('../lib/gift-18'),
+  '19':require('../lib/gift-19'),'20':require('../lib/gift-20')
+};
+module.exports=function(req,res){const id=String(req.query.id||'');const h=handlers[id];if(!h)return res.status(404).end();req.query.id=id;return h(req,res);};
